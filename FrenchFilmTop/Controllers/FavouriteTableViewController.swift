253b75e084d3f5d0fef2936316e76fc16779c2d7
developineta <77379211+developineta@users.noreply.github.com>
2021-11-26 11:20:49 +0200

    Favourites background image adjustment

--- FrenchFilmTop/Controllers/FavouriteTableViewController.swift
+++ FrenchFilmTop/Controllers/FavouriteTableViewController.swift
@@ -61,13 +61,9 @@ class FavouriteTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         if savedItems.count == 0 {
-            UIGraphicsBeginImageContext(self.view.frame.size)
-            UIImage(named: "paper-background.png")?.draw(in: self.view.bounds)
-            let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-            UIGraphicsEndImageContext()
-            self.view.backgroundColor = UIColor(patternImage: image)
+            tableView.setEmptyView(title: "You dont't have Favourite Films", message: "You can add your favourite films here")
         } else if savedItems.count > 0 {
-            self.view.backgroundColor = UIColor.darkGray
+            tableView.restore()
         }
         return savedItems.count
     }
@@ -128,5 +124,48 @@ class FavouriteTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
+    }
+}
+
+extension UITableView {
+    func setEmptyView(title: String, message: String) {
+        let emptyView = UIView(frame: CGRect(x: self.center.x, y: self.center.y, width: self.bounds.size.width, height: self.bounds.size.height))
+        let titleLabel = UILabel()
+        let messageLabel = UILabel()
+        let backgroundImage = UIImageView()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImage.image = UIImage(named: "paper-background")
+        backgroundImage.contentMode = .scaleAspectFit
+        titleLabel.textColor = UIColor.black
+        titleLabel.font = UIFont(name: "GeezaPro", size: 18)
+        messageLabel.textColor = UIColor.lightGray
+        messageLabel.font = UIFont(name: "GeezaPro", size: 17)
+        emptyView.addSubview(titleLabel)
+        emptyView.addSubview(messageLabel)
+        emptyView.addSubview(backgroundImage)
+        titleLabel.centerYAnchor.constraint(equalTo: emptyView.centerYAnchor).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+        
+        messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+        messageLabel.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 20).isActive = true
+        messageLabel.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -20).isActive = true
+        
+        backgroundImage.bottomAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+        backgroundImage.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 20).isActive = true
+        backgroundImage.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -20).isActive = true
+        
+        titleLabel.text = title
+        messageLabel.text = message
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        // The only tricky part is here:
+        self.backgroundView = emptyView
+        self.separatorStyle = .none
+    }
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
     }
 }
